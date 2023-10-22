2b53a21a112af88ced7b31bc28ab04f2d880812f
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2023-10-23 00:10:02 +0530

    added pattern5

--- src/week1/Patterns.java
+++ src/week1/Patterns.java
@@ -31,15 +31,29 @@ public class Patterns {
         // }
 
         //Pattern4
+        //  int n=6;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= n-i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+         //Pattern5
          int n=6;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n-i; j++) {
                 System.out.print(" ");
             }
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
+
+
     }
 }
