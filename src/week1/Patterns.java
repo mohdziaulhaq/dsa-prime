ef6b5a2a537e39383d05d7f53167894fb9f213db
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2023-10-23 01:18:11 +0530

    pattern 6 added

--- src/week1/Patterns.java
+++ src/week1/Patterns.java
@@ -43,13 +43,44 @@ public class Patterns {
         // }
 
          //Pattern5
+        //  int n=6;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= n-i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //pattern6
          int n=6;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 1; j <= n-i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+            System.out.print("*");
+            for (int j = 0; j <= 2*i-1; j++) {
+                if(j == 2*i-1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = n-2; i >= 0; i--) {
+             for (int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+             System.out.print("*");
+            for (int j = 0; j <= 2*i-1; j++) {
+                if(j == 2*i-1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
