bb2018f07cd52b745020bc016debc15db9f49ec0
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-01-25 00:32:15 -0600

    math and string

---
+++ src/javabasics/LearnString.java
@@ -0,0 +1,26 @@
+package javabasics;
+
+public class LearnString {
+    public static void main(String[] args) {
+        String name = "Zia";
+        String sameName = "Zia";
+        String newName = new String("Zia");
+
+        System.out.println(name);
+        System.out.println(sameName);
+
+        if(name == sameName){
+            System.out.println("Both are same");
+        }
+
+        if(name == newName){
+            System.out.println("Both are same");
+        }else{
+            System.out.println("Both are not same");
+        }
+
+        if(name.equals(newName)){
+            System.out.println("name and newName are same ");
+        }
+    }
+}
