4446aac88db71dfb66b9025588576916376b117f
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-06-08 06:50:53 -0500

    fibonacci

--- src/recursion/LearnRecursionBasics.java
+++ src/recursion/LearnRecursionBasics.java
@@ -3,7 +3,8 @@ package recursion;
 public class LearnRecursionBasics {
     public static void main(String[] args) {
         // fun(4);
-        System.out.println(sumOfN(5));
+        // System.out.println(sumOfN(5));
+        System.out.println(fibonacci(7));
     }
 
     static void fun(int n) {
@@ -19,5 +20,13 @@ public class LearnRecursionBasics {
         if (n == 1)
             return 1;
         return sumOfN(n - 1) + n;
+    }
+
+    static int fibonacci(int n) {
+        if (n == 0 || n == 1)
+            return n;
+
+        int result = fibonacci(n - 2) + fibonacci(n - 1);
+        return result;
     }
 }
